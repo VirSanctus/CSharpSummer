637825ea45b52abbb7143dc3709535c906761291
VirSanctus <aviprillywork@gmail.com>
2017-08-31 20:37:16 +0300

    initial commit

---
+++ Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
